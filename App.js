9f0d8b7a2f2f10d0a4c334e512d0f9413ccec71c
Vasundhara <85097331+vvasundharaa@users.noreply.github.com>
2024-06-08 14:17:24 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Button from './Counter'; 
+import Counter from './Counter'; 
 
 function App() {
   return (
     <div className="App">
-      <Button />
+      <Counter />
     </div>
   );
 }
